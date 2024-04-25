f08206dce0d536c186f1a036f6dbae840e89e80a
Hyundong Lee <137761528+hyundong-L@users.noreply.github.com>
2024-04-26 02:21:58 +0900

    [level 1] Title: 평균 구하기, Time: 3.07 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] arr) {
-        int i;
-        double answer = 0;
+//         int i;
+//         double answer = 0;
         
-        for(i=0;i<=arr.length-1;i++){
-            answer += arr[i];
-        }
+//         for(i=0;i<=arr.length-1;i++){
+//             answer += arr[i];
+//         }
         
-        return answer / i;
+//         return answer / i;
+        
+        return Arrays.stream(arr).average().orElse(0);
     }
 }

cf6031105aeede95d3c64a29ebbe1ce26690b7a0
Hyundong Lee <137761528+hyundong-L@users.noreply.github.com>
2024-07-02 20:23:34 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.20 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java) 
 
 ### 성능 요약
 
-메모리: 66.3 MB, 시간: 1.09 ms
+메모리: 74.5 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 03:11:43
+2024년 07월 02일 20:23:30
 
 ### 문제 설명
 
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,8 +1,14 @@
 class Solution {
     boolean solution(String s) {
-        s = s.toLowerCase();
-        
-        return s.chars().filter(c -> c == 'p').count() 
-            == s.chars().filter(c -> c == 'y').count();
+        int cnt_p = 0, cnt_y = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.toLowerCase().charAt(i);
+
+            if (character == 'p') cnt_p += 1;
+            else if (character == 'y') cnt_y += 1;
+        }
+
+        return cnt_p == cnt_y;
     }
 }

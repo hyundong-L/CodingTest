9ebf4fcc918f32adb2b1de16ac3cda019eb5172c
Hyundong Lee <137761528+hyundong-L@users.noreply.github.com>
2024-10-10 13:45:14 +0900

    Time: 5 ms (97.08%), Space: 44.1 MB (16.4%) - LeetHub

--- LeetCode/0179-largest-number/0179-largest-number.java
+++ LeetCode/0179-largest-number/0179-largest-number.java
@@ -2,11 +2,7 @@ class Solution {
     public String largestNumber(int[] nums) {
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                customSort(nums, i, j);
-            }
-        }
+        mergeSort(nums, 0, nums.length - 1);
 
         for (int num : nums) {
             result.append(num);
@@ -20,16 +16,69 @@ class Solution {
         return result.toString();
     }
 
-    //커스텀 정렬 메서드 -> 매개변수를 문자열로 더했을 때 더 큰 수가 나오도록 정렬
-    //compareTo() 사용
-    private void customSort(int[] list, int a, int b) {
+    private int[] mergeSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+
+            nums = mergeSort(nums, start, mid);
+            nums = mergeSort(nums, mid + 1, end);
+
+            return merge(nums, start, mid, end);
+        } else {
+            return nums;
+        }
+    }
+
+    private int[] merge(int[] nums, int start, int mid, int end) {
+        int left = start, right = mid + 1;
+        int[] result = new int[end - start + 1];
+        int index = 0;
+
+        while (left <= mid && right <= end) {
+            int compareValue = customCompareTo(nums, left, right);
+
+            if (compareValue > 0) {
+                result[index++] = nums[left++];
+            } else {
+                result[index++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) {
+            result[index++] = nums[left++];
+        }
+        while (right <= mid) {
+            result[index++] = nums[right++];
+        }
+
+        for (int i = 0; i < index; i++) {
+            nums[start + i] = result[i];
+        }
+
+        return nums;
+    }
+
+    // a > b -> 1
+    // a < b -> -1
+    // a == b -> 0
+    private int customCompareTo(int[] list, int a, int b) {
         String aCase = String.valueOf(list[a]) + String.valueOf(list[b]);
         String bCase = String.valueOf(list[b]) + String.valueOf(list[a]);
 
-        if (aCase.compareTo(bCase) < 0) {
-            int temp = list[a];
-            list[a] = list[b];
-            list[b] = temp;
+        if (aCase.length() == bCase.length()) {
+            for (int i = 0; i < aCase.length(); i++) {
+                if (aCase.charAt(i) == bCase.charAt(i)) {
+                    continue;
+                } else if (aCase.charAt(i) > bCase.charAt(i)) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+
+            return 0;
         }
+
+        return aCase.length() > bCase.length() ? 1 : -1;
     }
 }

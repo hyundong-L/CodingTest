fca0177e022cb7826291cf4e25c5e792f8f39afc
Hyundong Lee <137761528+hyundong-L@users.noreply.github.com>
2025-12-09 23:31:38 +0900

    Time: 0 ms (100%), Space: 12.3 MB (70.36%) - LeetHub

---
+++ LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        auto delete_index = m != 0 ? nums1.begin() + m : nums1.begin();
+        nums1.erase(delete_index, nums1.end());
+        
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+    }
+};
